0a710035eee700e89b91af6bfea4485b5714f76a
caio-tenorio <ccat@cin.ufpe.br>
2017-11-20 17:39:22 -0300

    Route para medicamentos#index, escondendo botao de login

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   
   devise_for :users
   resources :medicamentos
-  root "home#index"
+  root "medicamentos#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
